5c67996e6eebc09ff0a79baf99a5e3e3267d26e6
douglasbs <douglas-b.s@hotmail.com>
2014-10-22 17:23:19 -0200

    TESTE

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,12 +2,16 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
-
+//teste
 public class Application extends Controller {
 
     public static Result index() {
     	flash("sucesso", "");
         return ok(views.html.index.render());
+    }
+    
+    public static Result teste(){
+    	return TODO;
     }
 
 }
